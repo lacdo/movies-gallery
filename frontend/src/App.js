8074376b1772f7ff9818c2e9d063c9ad3514d1ba
Lac Vinh Do <lacvinhdo@gmail.com>
2022-10-05 13:41:35 +0100

    auto save with linter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
-import { useState, useTransition } from 'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
-
-useState()
 
 const App = () => {
   const [word, setWord] = useState('');
@@ -14,16 +11,18 @@ const App = () => {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-      .then((res => res.json()))
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
       .then((data) => {
         console.log(data);
       })
       .catch((err) => {
         console.log(err);
-      })
-    setWord('')
-  }
+      });
+    setWord('');
+  };
 
   return (
     <div>
@@ -31,6 +30,6 @@ const App = () => {
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
